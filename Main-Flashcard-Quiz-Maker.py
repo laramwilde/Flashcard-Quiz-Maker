1b19043d62ccdf6888e656952a3c17b1d6a32f1f
Lara Wilde <laramariewilde@gmail.com>
2025-12-01 20:33:01 +0000

    Made it so menu loops until quit. Added placeholder functions

--- Main-Flashcard-Quiz-Maker.py
+++ Main-Flashcard-Quiz-Maker.py
@@ -1,4 +1,5 @@
 def menu():
+    choice = None
     print("############################")
     print("    A FLASHCARD PROGRAM     ")
     print("############################")
@@ -21,9 +22,9 @@ def menu():
                     print("View Flashcards Selected")
                     #print("Please select a number between 1-3.")
                     print("###################################")
-                    print("1.View Flashcards")
-                    print("2.View Flashcards & edit one.")
-                    print("3.View Flashcards & delete")
+                    print("1.View Flashcards") # 2a
+                    print("2.View Flashcards & edit one.") # 2b
+                    print("3.View Flashcards & delete") #2c
                     while True:
                         choice = input("Please choose an option between 1-3.")
                         try:
@@ -32,11 +33,54 @@ def menu():
                             print("Please input a valid number, 1-3.")
                         else:
                             if 1 <= choice <= 3:
+                                #2a - View flashcards
+                                if choice == 1:
+                                    choice = "2a"
+                                #2b - View & Edit
+                                elif choice == 2:
+                                    choice = "2b"
+                                #2c - View & delete
+                                elif choice == 3:
+                                    choice = "2c"
                                 return choice
                 return choice
             else:
                 print("Please input a number between 1 and 4.")
 
-#def view_flashcards():
+def view_flashcards():
+    print("Placeholder")
 
-print(menu())
+def edit_flashcards():
+    print("Placeholder")
+
+def del_flashcards():
+    print("placeholder")
+
+def quiz_mode():
+    print("Placeholder")
+
+def view_scores():
+    print("Placeholder")
+
+while True:
+    choice = menu()
+    if choice == 1:
+        quiz_mode()
+
+    elif choice == "2a":
+        view_flashcards()
+
+    elif choice == "2b":
+        view_flashcards()
+        edit_flashcards()
+    
+    elif choice == "2c":
+        view_flashcards()
+        del_flashcards()
+    
+    elif choice == 3:
+        view_scores()
+
+    elif choice == 4:
+        break
+    
