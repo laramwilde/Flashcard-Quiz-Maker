323130b98eb2f5dddcf95301d3a4181478731f39
Lara Wilde <laramariewilde@gmail.com>
2025-12-02 00:26:35 +0000

    added a delete flashcards procedure, also neatened up menus as it was kinda hard to read

--- Main-Flashcard-Quiz-Maker.py
+++ Main-Flashcard-Quiz-Maker.py
@@ -10,7 +10,7 @@ def menu():
     # viewing flashcards will allow user to delete/edit them
     print("3.View Scores")
     print("4.Quit")
-    print("\nOption 2 will allow you to delete/edit flashcards.")
+    print("\nOption 2 will allow you to delete/edit flashcards.\n")
 
     while True:
         choice = input("Please choose an option by using a number 1-4: ")  
@@ -21,18 +21,18 @@ def menu():
         else:
             if 1 <= choice <= 4:
                 if choice == 2:
-                    print("View Flashcards Selected")
-                    #print("Please select a number between 1-3.")
+                    print("\n###################################")
+                    print("           View Flashcards           ")
                     print("###################################")
                     print("1.View Flashcards") # 2a
                     print("2.View Flashcards & edit one.") # 2b
                     print("3.View Flashcards & delete") #2c
                     while True:
-                        choice = input("Please choose an option between 1-3.")
+                        choice = input("Please choose an option between 1-3: ")
                         try:
                             choice = int(choice)
                         except ValueError:
-                            print("Please input a valid number, 1-3.")
+                            print("Please input a valid number, 1-3: ")
                         else:
                             if 1 <= choice <= 3:
                                 #2a - View flashcards
@@ -47,14 +47,16 @@ def menu():
                                 return choice
                 return choice
             else:
-                print("Please input a number between 1 and 4.")
+                print("Please input a number between 1 and 4: ")
 
 def view_flashcards():
     if not flashcards:
         print("Flashcards are empty!")
     else:
+        print("\nCurrent flashcards:")
         for term, definition in flashcards.items():
             print(term, ":", definition)
+        print("\n")
 
 def edit_flashcards():
     #makes a list of terms 
@@ -80,7 +82,26 @@ def edit_flashcards():
 
 
 def del_flashcards():
-    print("Place holder")
+    #prints each flashcard with a number
+    terms = list(flashcards.keys())
+    for i in range(len(terms)):
+        print(f"{i + 1}. {terms[i]}") # prints (Number. Term)
+    len_list = len(terms)
+    print("Which flashcard would you like to delete?")
+    while True:
+        user_choice = input(f"Choose a number 1-{len_list}")
+        try:
+            user_choice = int(user_choice)
+        except ValueError:
+            print("Please input a valid number.")
+        else:
+            if 1 <= user_choice >= len_list:
+                selected_index = user_choice - 1
+                selected_term = terms[selected_index]
+                del flashcards[selected_term]
+                print(f"Succesfully deleted {selected_term}")
+                break
+
 
 def quiz_mode():
     print("Placeholder")
@@ -97,7 +118,7 @@ while True:
         view_flashcards()
 
     elif choice == "2b":
-        view_flashcards()
+        view_flashcards() #this function needs to be called as edit flashcards only displays the terms, not definitions 
         edit_flashcards()
     
     elif choice == "2c":
